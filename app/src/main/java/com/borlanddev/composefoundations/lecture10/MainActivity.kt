dcbc6518902133c4fcb0d5813fbef65d8e3800bb
nikita.dubovik <flashes.n.co@gmail.com>
2024-01-17 18:33:19 +0200

    Finished lecture 10

---
+++ app/src/main/java/com/borlanddev/composefoundations/lecture10/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.borlanddev.composefoundations.lecture10
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppScreen()
+        }
+    }
+
+    @Preview(showSystemUi = true)
+    @Composable
+    fun AppScreen() {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            Container(name = "Buttons example") {
+                 ButtonsExample()
+            }
+            Container(name = "TextField example ") {
+                 TextFieldExample()
+            }
+            Container(name = "CheckBox example") {
+                CheckBoxesExample()
+            }
+        }
+    }
+}
